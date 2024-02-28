a1afdd9af123e004010a3b43f0a3fe78a0539d9c
AddChew <addison_chew@outlook.com>
2024-02-28 22:29:08 +0800

    add xgboost model

---
+++ src/xgboost/train.py
@@ -0,0 +1,10 @@
+import numpy as np
+import xgboost as xgb
+
+X = np.loadtxt("datasets/x.txt")
+y = np.loadtxt("datasets/y.txt")
+
+dataset = xgb.DMatrix(X, y)
+params = {'objective': 'multi:softmax', 'num_class': 3}
+model = xgb.train(params, dataset, 5)
+model.save_model("models/xgboost.json")
